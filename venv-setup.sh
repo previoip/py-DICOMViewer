79b83eb5a9aac9ea2e5b93a9b9fa8a65a11e4087
previoip <previo.prakasa@gmail.com>
2023-04-22 17:26:32 +0700

    revised venv-setup

--- venv-setup.sh
+++ venv-setup.sh
@@ -2,13 +2,11 @@
 
 set -be
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PY_VER="$(python --version)"
+SCRIPT_DIR=$(dirname $0)
 
-[[ "$PY_VER" =~ "Python 3" ]] && echo "Python 3 is installed"
-echo "using $PY_VER"
+echo $SCRIPT_DIR
 
-python -m venv "$SCRIPT_DIR"
-python -m pip install -r "$SCRIPT_DIR/requirements.txt"
+python3 -m venv "$SCRIPT_DIR"
+python3 -m pip install -r "$SCRIPT_DIR/requirements.txt"
 
 read -p "Press any key to resume ..."
