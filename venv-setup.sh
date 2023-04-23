8809def4da32c25dc7f7ff95daca9440a845613c
Previo Prakasa <previo.prakasa@gmail.com>
2023-04-23 16:53:12 +0700

    added venv package check to setup sh

--- venv-setup.sh
+++ venv-setup.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 set +e
 
+echo "==================================================="
 SCRIPT_DIR=$(dirname $0)
 
+PY_VER=$(python.exe --version)
+PY_PACKAGE_VENV=$(python.exe -m pip list | grep "virtualenv")
+
+echo "using python:" $PY_VER
+echo "using venv:" $PY_PACKAGE_VENV
+
+if [ -n $PY_PACKAGE_VENV ]; then
+  echo "venv is not installed. Installing venv"
+  python.exe -m pip install virtualenv
+fi
+
+echo
 echo "building venv" $SCRIPT_DIR
 python.exe -m venv "$SCRIPT_DIR"
-
 
 echo
 if [ -d "bin" ]; then
